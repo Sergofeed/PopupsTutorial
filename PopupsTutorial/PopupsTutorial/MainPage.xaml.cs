574f7d2b759e3c17b01b4caba6d0f78011ca28f0
Sergofeed <serzhik.ppp223@gmail.com>
2021-12-07 22:16:39 +0300

    Добавьте файлы проекта.

---
+++ PopupsTutorial/PopupsTutorial/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PopupsTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void OnDisplayActionSheetButtonClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+            Console.WriteLine("Action: " + action);
+        }
+
+        async void OnDisplayAlertQuestionButtonClicked(object sender, EventArgs e)
+        {
+            bool response = await DisplayAlert("Save?", "Would you like to save your data?", "Yes", "No");
+            Console.WriteLine("Save data: " + response);
+        }
+    }
+}
